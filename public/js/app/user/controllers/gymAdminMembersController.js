1600ef8af3a82b4dafe577520d8e397e028c3538
Bruno Alexandre <bruce_alex2006@hotmail.com>
2015-09-10 19:47:35 -0300

    Ajuste no modal alunos para tratamento logico em admin

--- public/js/app/user/controllers/gymAdminMembersController.js
+++ public/js/app/user/controllers/gymAdminMembersController.js
@@ -1,11 +1,11 @@
 var fs = require('fs');
 module.exports = function($scope,$timeout,fbUtil,$mdDialog) {
-	$scope.staffDialog = function(ev) {
+	$scope.newMemberDialog = function(ev) {
 		$mdDialog.show({
 			controller: 'membersAdminDialogController',
 			// scope: $scope,
 			// preserveScope: true,
-			locals: {bruno:"Alexandre"},
+			locals: null,
 			// bindToController: true,
 			template: fs.readFileSync(__dirname + '/../templates/membersAdminDialogTemplate.html'),
 			parent: angular.element(document.body),
@@ -20,5 +20,34 @@ module.exports = function($scope,$timeout,fbUtil,$mdDialog) {
 			// }
 		});
 	};
+	$scope.editMemberDialog = function(ev,member) {
+		var member2 = {
+				name: 'Membro',
+				surname: '',
+				email: '',
+				image: '',
+				birthday: '',
+				phone:'',
+		};
+		$mdDialog.show({
+			controller: 'membersAdminDialogController',
+			// scope: $scope,
+			// preserveScope: true,
+			locals: member2,
+			// bindToController: true,
+			template: fs.readFileSync(__dirname + '/../templates/membersAdminDialogTemplate.html'),
+			parent: angular.element(document.body),
+			targetEvent: ev,
+		})
+		.then(function(answer) {
+			// $scope.updateGym();
+		}, function() {
+			// if (Object.keys(changeList).length !== 0) {
+			// 	$scope.gym = gymBackup;
+			// 	gymBackup = {};
+			// }
+		});
+	};
+	$scope.lista = [1,2,3];
 	$scope.lista = [1,2,3];
 };
--- public/js/app/user/controllers/membersAdminDialogController.js
+++ public/js/app/user/controllers/membersAdminDialogController.js
@@ -1,14 +1,36 @@
-module.exports = function($scope,bruno,$mdDialog) {
-	// console.log(gymBackup);
-	// $scope.save = function() {
-	// 	$mdDialog.hide();
-	// };
-	console.log(bruno);
-	console.log($scope.lista);
+module.exports = function($scope,locals,$mdDialog) {
+
+	var changeList = {};
+
+	$scope.member = {
+			name: '',
+			surname: '',
+			email: '',
+			image: '',
+			birthday: '',
+			phone:'',
+	};
+
+	$scope.mode = "Inserção";
+
+	var init = function (){
+		if (locals !== null) {
+			$scope.mode = "Edição";
+			$scope.member = locals;
+		}
+	};
+	$scope.change = function(key) {
+		if ($scope.mode === "Edição") {
+			changeList[key] = $scope.member[key];
+			console.log(changeList);
+		}
+	};
 	$scope.cancel = function() {
 		$mdDialog.cancel();
 	};
 	$scope.save = function(answer) {
 		$mdDialog.hide(answer);
 	};
+
+	init();
 };
