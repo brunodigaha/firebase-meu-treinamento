b4cb9274c11b740ea2b1ee1b09dc18adbc204192
Bruno Alexandre <bruce_alex2006@hotmail.com>
2015-09-10 18:41:10 -0300

    Criação inicial da lógica do modal de funcionários para edição e inserção de funcionários

--- public/js/app/user/controllers/gymAdminStaffController.js
+++ public/js/app/user/controllers/gymAdminStaffController.js
@@ -1,11 +1,11 @@
 var fs = require('fs');
 module.exports = function($scope,$timeout,fbUtil,$mdDialog) {
-	$scope.staffDialog = function(ev) {
+	$scope.newStaffDialog = function(ev) {
 		$mdDialog.show({
 			controller: 'staffAdminDialogController',
 			// scope: $scope,
 			// preserveScope: true,
-			locals: {bruno:"Alexandre"},
+			locals: null,
 			// bindToController: true,
 			template: fs.readFileSync(__dirname + '/../templates/staffAdminDialogTemplate.html'),
 			parent: angular.element(document.body),
@@ -20,5 +20,34 @@ module.exports = function($scope,$timeout,fbUtil,$mdDialog) {
 			// }
 		});
 	};
+	$scope.editStaffDialog = function(ev,staff) {
+		var staff2 = {
+			name: 'Brun',
+			surname: '',
+			email: '',
+			birthday: '',
+			phone:'',
+			permission: ''
+		};
+		$mdDialog.show({
+			controller: 'staffAdminDialogController',
+			// scope: $scope,
+			// preserveScope: true,
+			locals: staff2,
+			// bindToController: true,
+			template: fs.readFileSync(__dirname + '/../templates/staffAdminDialogTemplate.html'),
+			parent: angular.element(document.body),
+			targetEvent: ev,
+		})
+		.then(function(answer) {
+			// $scope.updateGym();
+		}, function() {
+			// if (Object.keys(changeList).length !== 0) {
+			// 	$scope.gym = gymBackup;
+			// 	gymBackup = {};
+			// }
+		});
+	};
+	$scope.lista = [1,2,3];
 	$scope.lista = [1,2,3];
 };
--- public/js/app/user/controllers/staffAdminDialogController.js
+++ public/js/app/user/controllers/staffAdminDialogController.js
@@ -1,14 +1,36 @@
-module.exports = function($scope,bruno,$mdDialog) {
-	// console.log(gymBackup);
-	// $scope.save = function() {
-	// 	$mdDialog.hide();
-	// };
-	console.log(bruno);
-	console.log($scope.lista);
+module.exports = function($scope,locals,$mdDialog) {
+
+	var changeList = {};
+
+	$scope.staff = {
+			name: '',
+			surname: '',
+			email: '',
+			birthday: '',
+			phone:'',
+			permission: ''
+	};
+
+	$scope.mode = "Inserção";
+
+	var init = function (){
+		if (locals !== null) {
+			$scope.mode = "Edição";
+			$scope.staff = locals;
+		}
+	};
 	$scope.cancel = function() {
 		$mdDialog.cancel();
 	};
 	$scope.save = function(answer) {
 		$mdDialog.hide(answer);
 	};
+	$scope.change = function(key) {
+		if ($scope.mode === "Edição") {
+			changeList[key] = $scope.staff[key];
+			console.log(changeList);
+		}
+	};
+
+	init();
 };
