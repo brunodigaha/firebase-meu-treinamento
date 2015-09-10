50e78b58188884cd9d1a902388051216a73afa9a
Bruno Alexandre <bruce_alex2006@hotmail.com>
2015-09-10 14:16:33 -0300

    Soluçãop do problema scroll em alunos por causa do angularTooltip

--- public/js/app/appDirectives/directives/dsUserDirective.js
+++ public/js/app/appDirectives/directives/dsUserDirective.js
@@ -2,6 +2,7 @@ var fs = require('fs');
 module.exports = function() {
 	return{
 		template: fs.readFileSync(__dirname + '/../templates/dsUserTemplateDirective.html'),
+		transclude:true,
 		// scope: {
 		// },
 		// controller: function($scope,$log,$element,$attrs,exercisePatternModel){
